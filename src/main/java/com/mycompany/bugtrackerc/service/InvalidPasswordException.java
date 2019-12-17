60b9b7290ec9d091488f8cd50b82be3d66fd3389
John Doe <johndoe@example.com>
2019-12-17 13:16:09 -0700

    Initial version of BugTrackerJHipster generated by JHipster-6.5.1

---
+++ src/main/java/com/mycompany/bugtrackerc/service/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package com.mycompany.bugtrackerc.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
---
+++ src/main/java/com/mycompany/bugtrackerc/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.mycompany.bugtrackerc.web.rest;
